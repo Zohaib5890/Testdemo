81d2aea873bf25b1be65cf8f913ac97e16b1125c
Zohaib Ahmad <129852755+Zohaib5890@users.noreply.github.com>
2023-08-19 21:55:31 +0500

    Revert "New website"
    This reverts commit 4abfc1452c4b55363f6cf9a9a066eb4046bc1da4.

--- wp-content/themes/flash-news/sections/above-footer-widgets.php
+++
@@ -1,7 +0,0 @@
-<?php if ( is_active_sidebar( 'above-footer-widgets-section' ) ) : ?>
-	<div class="above-footer-widgets-section">
-		<div class="ascendoor-wrapper">
-			<?php dynamic_sidebar( 'above-footer-widgets-section' ); ?>
-		</div>
-	</div>
-<?php endif; ?>
